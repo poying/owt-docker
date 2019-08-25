574ce0df051bf2b930ad2b4e5420c999fdd2e98b
Po-Ying Chen <poying.me@gmail.com>
2019-08-25 21:48:17 +0800

    create super service

--- owt-server/start.sh
+++ owt-server/start.sh
@@ -2,7 +2,9 @@
 
 service rabbitmq-server start \
     && cd /owt-server/dist/bin \
+    && /owt-server/dist/management_api/init.sh --dburl=mongo/owtdb \
     && cp /config/portal.toml /owt-server/dist/portal/portal.toml \
     && cp /config/management_api.toml /owt-server/dist/management_api/management_api.toml \
+    && cp /config/management_console.toml /owt-server/dist/management_console/management_console.toml \
     && ./start-all.sh \
     && sleep infinity
