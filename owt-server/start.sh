9c72f38b3264d599b77d196bf3ca3b21b5015b7d
Po-Ying Chen <poying.me@gmail.com>
2019-08-25 21:05:21 +0800

    fix owt configuration

---
+++ owt-server/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+service rabbitmq-server start \
+    && cd /owt-server/dist/bin \
+    && cp /config/portal.toml /owt-server/dist/portal/portal.toml \
+    && cp /config/management_api.toml /owt-server/dist/management_api/management_api.toml \
+    && ./start-all.sh \
+    && sleep infinity
